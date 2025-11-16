f40d65dde0715d13f45884b0843a2ff35a307509
FloofHips <61808556+FloofHips@users.noreply.github.com>
2025-11-16 20:20:21 +0100

    fall through

--- src/main/java/com/thebeyond/common/block/AuroraciteBlock.java
+++ src/main/java/com/thebeyond/common/block/AuroraciteBlock.java
@@ -1,10 +1,15 @@
 package com.thebeyond.common.block;
 
+import com.thebeyond.common.entity.AbyssalNomadEntity;
 import com.thebeyond.common.registry.BeyondParticleTypes;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -12,6 +17,10 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.EntityCollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class AuroraciteBlock extends Block {
     public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
@@ -19,6 +28,32 @@ public class AuroraciteBlock extends Block {
     public AuroraciteBlock(Properties properties) {
         super(properties);
         registerDefaultState(this.stateDefinition.any().setValue(POWERED, false));
+    }
+    @Override
+    protected VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        if (context instanceof EntityCollisionContext entitycollisioncontext) {
+            Entity entity = entitycollisioncontext.getEntity();
+            if (entity != null) {
+                if (!(canEntityWalkOn(entity) && context.isAbove(Shapes.block(), pos, false))) {
+                    return Shapes.empty();
+                }
+            }
+        }
+
+        return Shapes.block();
+    }
+
+    private boolean canEntityWalkOn(Entity entity) {
+        if (entity instanceof AbyssalNomadEntity) {
+            return true;
+        } else {
+            return entity instanceof LivingEntity ? ((LivingEntity)entity).getItemBySlot(EquipmentSlot.FEET).is(Items.GOLDEN_BOOTS) : false;
+        }
+    }
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        if (Minecraft.getInstance().player != null && !Minecraft.getInstance().player.isCreative()) return Shapes.empty();
+        return Shapes.block();
     }
 
     @Override
--- src/main/java/com/thebeyond/common/entity/AbyssalNomadEntity.java
+++ src/main/java/com/thebeyond/common/entity/AbyssalNomadEntity.java
@@ -1,10 +1,13 @@
 package com.thebeyond.common.entity;
 
+import com.thebeyond.common.registry.BeyondBlocks;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.behavior.FollowTemptation;
+import net.minecraft.world.entity.ai.goal.FollowMobGoal;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
 import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
@@ -12,6 +15,7 @@ import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class AbyssalNomadEntity extends PathfinderMob {
     public AbyssalNomadEntity(EntityType<? extends PathfinderMob> entityType, Level level) {
@@ -27,5 +31,24 @@ public class AbyssalNomadEntity extends PathfinderMob {
         this.goalSelector.addGoal(0, new LookAtPlayerGoal(this, Player.class, 3f));
 
         this.targetSelector.addGoal(0, new NearestAttackableTargetGoal<>(this, EnderglopEntity.class, true));
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if (tickCount % 100 == 0) {
+            checkInsideBlocks();
+        }
+        this.navigation.moveTo(0,0,0,1);
+        //if (tickCount % 20 == 0) {
+        //    this.navigation.stop();
+        //}
+    }
+
+    @Override
+    protected void onInsideBlock(BlockState state) {
+        super.onInsideBlock(state);
+        if (state.is(BeyondBlocks.AURORACITE))
+            this.setDeltaMovement(0, 0.5f, 0);
     }
 }
